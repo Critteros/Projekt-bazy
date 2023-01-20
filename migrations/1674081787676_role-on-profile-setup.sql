a5d3c5240638f3eae5a7727dff65aa6f162be3b0
Szymon Kozioł <szymonkoz@student.agh.edu.pl>
2023-01-20 16:31:13 +0100

    Added bundle tracker and trigger for customer inserts

---
+++ migrations/1674081787676_role-on-profile-setup.sql
@@ -0,0 +1,52 @@
+-- Up Migration
+BEGIN;
+
+CREATE FUNCTION assign_customer_role() RETURNS TRIGGER AS
+$$
+DECLARE
+    customer_role_id integer;
+BEGIN
+
+    -- If customer record is being inserted with null account id then do nothing
+    IF (TG_OP = 'INSERT' AND NEW.account_id IS NULL) THEN
+        RETURN NEW;
+    END IF;
+
+    -- Get the id for the customer role
+    BEGIN
+        SELECT role_id INTO STRICT customer_role_id FROM account_roles WHERE name = 'customer';
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE EXCEPTION 'role customer could not be found, fatal error';
+    END;
+
+    -- Check if the user have already the customer role (this should be not really possible)
+    IF (TG_OP = 'INSERT' AND
+        EXISTS(SELECT 1
+               FROM account_role_relation
+               WHERE account_id = NEW.account_id
+                 AND role_id = customer_role_id)) THEN
+        RETURN NEW;
+    END IF;
+
+    -- Insert a customer role for the user
+    IF (TG_OP = 'INSERT') THEN
+        INSERT INTO account_role_relation (account_id, role_id) VALUES (NEW.account_id, customer_role_id);
+    END IF;
+
+    RETURN NEW;
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER assign_customer_role
+    AFTER INSERT OR UPDATE
+    ON customer
+    FOR EACH ROW
+EXECUTE PROCEDURE assign_customer_role();
+
+COMMIT;
+
+
+-- Down Migration
+DROP FUNCTION assign_customer_role() CASCADE;
--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,20 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'));
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
 };
-export default config;
+
+const withBundleAnalyzer = (await import('@next/bundle-analyzer')).default({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default withBundleAnalyzer(config);
